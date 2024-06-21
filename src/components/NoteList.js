c00d02b34c56688693104264a866682e5e54bd46
qwq140 <qwq140@gmail.com>
2024-06-22 00:32:10 +0900

    리스트 디자인 수정

--- src/components/NoteList.js
+++ src/components/NoteList.js
@@ -3,16 +3,16 @@ import {Link} from "react-router-dom";
 
 const NoteList = ({notes}) => {
     return (
-        <div className="note-list-container">
+        <>
             <ul className="note-list">
                 {notes.map(note => (
-                    <li key={note.id}>
-                        <Link to={`/detail/${note.id}`}>{note.title}</Link>
+                    <Link to={`/detail/${note.id}`} key={note.id} className="note-item">
+                        <h2>{note.title}</h2>
                         <p>last edited {note.lastEdited}</p>
-                    </li>
+                    </Link>
                 ))}
             </ul>
-        </div>
+        </>
     )
 }
 
